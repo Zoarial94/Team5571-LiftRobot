de6b7d576d945b63c82ddabf5e775d9e8f67e4c1
Zoarial94 <hunterw9469@gmail.com>
2018-11-14 17:18:26 -0500

    Added empty file for claw subsystem

---
+++ LiftRobot/src/org/usfirst/frc/team5571/robot/subsystems/ClawSubsystem.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team5571.robot.subsystems;
+
+import org.usfirst.frc.team5571.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Spark;
+
+/**
+ *
+ */
+public class ClawSubsystem extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
